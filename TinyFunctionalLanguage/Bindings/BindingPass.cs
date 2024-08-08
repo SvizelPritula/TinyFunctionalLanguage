c66445f8ddae915b3df416fec6c6ec48a280c178
Benjamin Swart <Benjaminswart@email.cz>
2024-08-08 17:55:58 +0200

    Refactor BindingPass class

--- TinyFunctionalLanguage/Bindings/BindingPass.cs
+++ TinyFunctionalLanguage/Bindings/BindingPass.cs
@@ -12,61 +12,72 @@ public static class BindingPass
         {
             if (decl is FunctionDecl functionDecl)
             {
-                List<Argument> arguments = [];
-
-                foreach (ArgumentDecl argDecl in functionDecl.Arguments)
-                {
-                    Argument arg = new(argDecl.Ident.Name);
-                    argDecl.Reference = arg;
-                    arguments.Add(arg);
-                }
-
-                Function function = new(arguments);
-
-                functionDecl.Reference = function;
-                scope.Insert(functionDecl.Ident.Name, function);
+                AttachFunctionObjects(functionDecl);
+                scope.Insert(functionDecl.Ident.Name, functionDecl.Reference!);
             }
             else if (decl is StructDecl structDecl)
             {
-                List<Field> fields = [];
-
-                foreach (FieldDecl fieldDecl in structDecl.Fields)
-                {
-                    Field field = new(fieldDecl.Ident.Name);
-                    fieldDecl.Reference = field;
-                    fields.Add(field);
-                }
-
-                Struct @struct = new(fields);
-
-                structDecl.Reference = @struct;
-                scope.Insert(structDecl.Ident.Name, @struct);
+                AttachStructObjects(structDecl);
+                scope.Insert(structDecl.Ident.Name, structDecl.Reference!);
             }
         }
 
-        BindingPassVisitor visitor = new(scope);
-
         foreach (FunctionDecl funcDecl in program.Functions)
-        {
-            foreach (var argDecl in funcDecl.Arguments)
-                argDecl.Type.Accept(visitor);
-            funcDecl.ReturnType.Accept(visitor);
-
-            Function func = funcDecl.Reference!;
-            scope.Push();
-
-            foreach (var (arg, argDecl) in func.Arguments.Zip(funcDecl.Arguments))
-                scope.Insert(argDecl.Ident.Name, arg);
-
-            funcDecl.Block.Accept(visitor);
-
-            scope.Pop();
-        }
+            BindFunction(scope, funcDecl);
 
         foreach (StructDecl structDecl in program.Structs)
         {
+            BindingPassVisitor visitor = new(scope);
             foreach (var fieldDecl in structDecl.Fields)
                 fieldDecl.Type.Accept(visitor);
         }
+    }
+
+    static void AttachFunctionObjects(FunctionDecl decl)
+    {
+        List<Argument> arguments = [];
+
+        foreach (ArgumentDecl argDecl in decl.Arguments)
+        {
+            Argument arg = new(argDecl.Ident.Name);
+            argDecl.Reference = arg;
+            arguments.Add(arg);
+        }
+
+        decl.Reference = new(arguments);
+    }
+
+    static void AttachStructObjects(StructDecl decl)
+    {
+        List<Field> fields = [];
+
+        foreach (FieldDecl fieldDecl in decl.Fields)
+        {
+            Field field = new(fieldDecl.Ident.Name);
+            fieldDecl.Reference = field;
+            fields.Add(field);
+        }
+
+        decl.Reference = new(fields);
+    }
+
+    static void BindFunction(ScopedMap<string, IBindable> scope, FunctionDecl funcDecl)
+    {
+
+        BindingPassVisitor visitor = new(scope);
+
+        foreach (var argDecl in funcDecl.Arguments)
+            argDecl.Type.Accept(visitor);
+        funcDecl.ReturnType.Accept(visitor);
+
+        Function func = funcDecl.Reference!;
+        scope.Push();
+
+        foreach (var (arg, argDecl) in func.Arguments.Zip(funcDecl.Arguments))
+            scope.Insert(argDecl.Ident.Name, arg);
+
+        funcDecl.Block.Accept(visitor);
+
+        scope.Pop();
     }
 }
